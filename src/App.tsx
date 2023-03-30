83759ec1caf0efdacf07235d38295814de1dee6f
Kelvin You <ykekelvin0220@gmail.com>
2023-03-30 15:40:24 +0800

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Sidebar, Menu, MenuItem, SubMenu, useProSidebar } from 'react-pro-sidebar';
 
 function App() {
+  const { collapseSidebar, toggleSidebar, collapsed, toggled, broken, rtl } = useProSidebar();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: 'flex', height: '100%' }}>
+      <Sidebar>
+        <Menu>
+        <SubMenu label="Charts">
+          <MenuItem> Pie charts </MenuItem>
+          <MenuItem> Line charts </MenuItem>
+          <SubMenu label="Charts">
+            <MenuItem> Pie charts </MenuItem>
+            <MenuItem> Line charts </MenuItem>
+            <SubMenu label="Charts">
+              <MenuItem> Pie charts </MenuItem>
+              <MenuItem> Line charts </MenuItem>
+              <SubMenu label="Charts">
+                <MenuItem> Pie charts </MenuItem>
+                <MenuItem> Line charts </MenuItem>
+
+              </SubMenu>
+            </SubMenu>
+          </SubMenu>
+        </SubMenu>
+        <MenuItem> Documentation </MenuItem>
+        <MenuItem> Calendar </MenuItem>
+        </Menu>
+      </Sidebar>
+      <main>
+        <button onClick={() => collapseSidebar()}>Collapse</button>
+      </main>
     </div>
   );
 }
